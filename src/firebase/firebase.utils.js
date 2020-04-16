3dc889de03ac51766f2571697e4d46a7aa0b8470
Alfonso Chavez Negrete <alfonso.chavez.n@gmail.com>
2020-04-15 23:23:27 -0600

    Added signin with google functionality

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDCgbUXHRvHZPyQ8sVQbThEySZYVPYI2ho",
+    authDomain: "crwn-db-aca3b.firebaseapp.com",
+    databaseURL: "https://crwn-db-aca3b.firebaseio.com",
+    projectId: "crwn-db-aca3b",
+    storageBucket: "crwn-db-aca3b.appspot.com",
+    messagingSenderId: "249338164955",
+    appId: "1:249338164955:web:db21c3d376b904449ebefe",
+    measurementId: "G-8HT5RP050D"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
