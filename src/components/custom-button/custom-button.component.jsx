e3e5b58f8cf888650c2944f4f7bde56c58e71b2c
Alfonso Chavez Negrete <alfonso.chavez.n@gmail.com>
2020-05-02 21:56:17 -0600

    updated shop data, created cart utils for cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, isFacebookSignIn, isTwitterSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, isFacebookSignIn, isTwitterSignIn, inverted, ...otherProps }) => (
     <button 
-        className={`${isGoogleSignIn ? 'google-sign-in' : 
+        className={`${inverted? 'inverted' : ''} 
+        ${isGoogleSignIn ? 'google-sign-in' : 
             isFacebookSignIn ? 'facebook-sign-in' : 
             isTwitterSignIn ? 'twitter-sign-in' : ''} custom-button`} 
         { ...otherProps }
