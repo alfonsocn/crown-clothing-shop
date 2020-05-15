02fe5943166116ba19b01d8d6141e59d0a8a288b
Alfonso Chavez Negrete <alfonso.chavez.n@gmail.com>
2020-05-14 22:52:46 -0600

    moved shop collections to redux and selectors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
 import './shop.styles.scss';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {
+            collections.map(({id, ...collectionProps}) => (
+                <CollectionPreview key={id} {...collectionProps}/>
+            ))
         }
-    }
+    </div>
+);
 
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...collectionProps}) => (
-                        <CollectionPreview key={id} {...collectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
